901db23ffee4e0e1df8a7b5a1da5b9066f634bec
weltrusten <philentropy@protonmail.ch>
2021-04-14 13:57:31 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="col-12 mccs-footer">
+    <nav class="col-12 mccs-nav-footer">
+        <ul>
+            <li><a href="#">About</a>
+            <li><a href="#">About</a>
+            <li><a href="#">About</a>
+        </ul>
+    </nav>
+</div>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>Home - MCCS</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <nav class="col-sm-8 col-md-8 col-lg-8 col-xl-8 nav">
+                    <span id="title">My Code Cheatsheets</span>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="html.php">HTML</a></li>
+                        <li><a href="css.php">CSS</a></li>
+                        <li><a href="javascript.php">Javascript</a></li>
+                        <li><a href="jquery.php">JQuery</a></li>
+                        <li><a href="php.php">PHP</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+
+            </div>
+
+        </div>
+    </main>
+</body>
+
+</html>
